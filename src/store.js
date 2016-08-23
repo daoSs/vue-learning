e1b7a0d11ac0a436e3ed1641d391f1d8fabc9206
shuoxu <514197939@qq.com>
2016-08-23 23:46:29 +0800

    vue.js

--- src/store.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = 'todos-vuejs'
-export default {
-	fetch(){
-		return JSON.parse(window.localstorage.getItem(STORAGE_KEY) || '[]');
-	},
-	save(items){
-		window.localstorage.setItem(STORAGE_KEY , JSON.stringify(items))
-	}
-}
