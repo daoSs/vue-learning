5dae9b5c1d2621b0352e7aafe87b2149c95758f9
shuoxu <514197939@qq.com>
2016-08-23 23:32:08 +0800

    vue

---
+++ vue_todo_list/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+      <h1 v-html="title"></h1>
+      <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+       <li v-for="item in items" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">
+           {{item.label}}
+       </li>
+    </ul>
+    <conp-a msgfromfather='memeda'></conp-a>
+  </div>
+</template>
+
+<script>
+import Store from './store'
+import ConpA from './components/conp'
+export default {
+  data: function() {
+      return {
+        title: 'this is a todo list',
+        items: Store.fetch(),
+        newItem: ''
+      }
+     
+  },
+  components:{ConpA},
+  watch: {
+      items:{
+        handler: function(items){
+          Store.save(items);
+        },
+        deep:true
+      }
+  },
+  methods: {
+    toggleFinish: function(item){
+      item.isFinished = !item.isFinished;
+    },
+    addNew: function(){
+      this.items.push({
+        label: this.newItem,
+        isFinished:false
+      })
+      this.newItem='';
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+.finished{
+  text-decoration: underline;
+  list-style: none;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
